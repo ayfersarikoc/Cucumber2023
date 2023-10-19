47e10a85557d321b11c8404c8af5cf70cbd5679a
ayfersarikoc <ayfersarikoc76@gmail.com>
2023-10-19 11:54:24 +0100

    19/10/2023

---
+++ src/test/java/stepdefinitions/GoogleStepdefinitions.java
@@ -0,0 +1,30 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import pages.GooglePage;
+import utilities.Driver;
+
+public class GoogleStepdefinitions {
+    GooglePage googlePage=new GooglePage();
+
+
+    @Given("cookies kabul eder")
+    public void cookies_kabul_eder() {
+
+    }
+    @Then("google da {string} icin arama yapar")
+    public void google_da_icin_arama_yapar(String istenenKelime) {
+        googlePage.googleSearchBox.sendKeys(istenenKelime+ Keys.ENTER);
+
+    }
+    @Then("google arama sonuclarinin {string} icerdigini test eder")
+    public void google_arama_sonuclarinin_icerdigini_test_eder(String istenenKelime) {
+        String actualTitle= Driver.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.contains(istenenKelime));
+
+    }
+
+}
